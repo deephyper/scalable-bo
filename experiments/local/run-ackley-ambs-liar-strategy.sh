43908367e8476aaa6858d604f33c6d52bee6e441
Deathn0t <romainegele@gmail.com>
2022-03-03 10:18:22 +0100

    local script for ambs async

---
+++ experiments/local/run-ackley-ambs-liar-strategy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export timeout=5
+export RANDOM_STATES=(42 2022 1451 8317 213 7607 4978 1516 2335 3366)
+export PROBLEMS=("ackley") 
+export LIAR_STRATEGIES=("cl_max" "boltzmann")
+
+# AMBS + Liar Strategy
+for liar_strategy in ${LIAR_STRATEGIES[@]}; do
+    for random_state in ${RANDOM_STATES[@]}; do
+        for problem in ${PROBLEMS[@]}; do
+            export log_dir="output/$problem-amsb-async-$liar_strategy-1-8-$timeout-$random_state";
+            echo "Running: mpirun -np 8 python -m scalbo.exp --problem $problem \
+                --search AMBS \
+                --timeout $timeout \
+                --liar-strategy $liar_strategy \
+                --random-state $random_state \
+                --log-dir $log_dir \
+                --verbose 1";
+            mpirun -np 8 python -m scalbo.exp --problem $problem \
+                --search AMBS \
+                --timeout $timeout \
+                --liar-strategy $liar_strategy \
+                --random-state $random_state \
+                --log-dir $log_dir \
+                --verbose 1 
+        done
+    done
+done

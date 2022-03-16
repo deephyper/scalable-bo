a8ee67232f99f60d736eb23330f342707b74f68b
Deathn0t <romainegele@gmail.com>
2022-03-16 17:28:41 +0100

    updated mig mode test

--- experiments/thetagpu/mig-example.sh
+++ experiments/thetagpu/mig-example.sh
@@ -5,19 +5,25 @@
 #COBALT -A datascience
 #COBALT --attrs mig-mode=True
 
-export DEVICES=(0 1 2 3 4 5 6 7)
+touch nvidia-smi-mig-lgip.txt
+nvidia-smi mig -lgip > nvidia-smi-mig-lgip.txt
+
+touch nvidia-smi-mig-i0-lgip.txt
+nvidia-smi mig -i 0 -lgip > nvidia-smi-mig-i0-lgip.txt
+
+export DEVICES=(0)
 
 for device_id in ${DEVICES[@]}; do
     # Creates GPU instances (GI)
-    nvidia-smi mig -i $device_id -cgi 9,9;
+    nvidia-smi mig -i $device_id -cgi 9,9 &&
 
     # Creates Compute instances (CI)
-    nvidia-smi mig -i $device_id -gi 1 -cci 0;
-    nvidia-smi mig -i $device_id -gi 2 -cci 0;
+    nvidia-smi mig -i $device_id -gi 1 -cci 0 &&
+    nvidia-smi mig -i $device_id -gi 2 -cci 0 &&
 done
 
-touch nvidia-smi.txt
-nvidia-smi -L > nvidia-smi.txt
+touch nvidia-smi-L.txt
+nvidia-smi -L > nvidia-smi-L.txt
 
 # GPU 0: A100-SXM4-40GB (UUID: GPU-2fa5cf01-9520-1bc1-995b-cc0c6d9bca7c)
 #   MIG 1c.3g.20gb Device 0: (UUID: MIG-GPU-2fa5cf01-9520-1bc1-995b-cc0c6d9bca7c/1/0)

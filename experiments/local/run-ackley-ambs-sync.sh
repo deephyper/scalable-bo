5caa61a62e27a5e303edfb0516a2c2e8b59c0e04
Deathn0t <romainegele@gmail.com>
2022-03-17 09:34:57 +0100

    updated ackley local

--- experiments/local/run-ackley-ambs-sync.sh
+++ experiments/local/run-ackley-ambs-sync.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-# export timeout=5
 # export RANDOM_STATES=(42 2022 1451 8317 213 7607 4978 1516 2335 3366)
-# export PROBLEMS=("ackley") 
-export LIAR_STRATEGIES=("boltzmann")
-
+export STRATEGIES=("cl_max")
 export timeout=30
 export RANDOM_STATES=(42)
 export PROBLEMS=("ackley") 
-# export LIAR_STRATEGIES=("topk")
+export acq_func="UCB"
 
 # AMBS + Liar Strategy
-for liar_strategy in ${LIAR_STRATEGIES[@]}; do
+for strategy in ${STRATEGIES[@]}; do
     for random_state in ${RANDOM_STATES[@]}; do
         for problem in ${PROBLEMS[@]}; do
-            export log_dir="output/$problem-ambs-sync-$liar_strategy-1-8-$timeout-$random_state";
+            export log_dir="output/$problem-ambs-sync-$acq_func-$strategy-1-8-$timeout-$random_state";
             echo "Running: mpirun -np 8 python -m scalbo.exp --problem $problem \
             --search AMBS \
             --timeout $timeout \
-            --liar-strategy $liar_strategy \
+            --acq-func $acq_func \
+            --strategy $strategy \
             --random-state $random_state \
             --log-dir $log_dir \
             --verbose 1 \
@@ -26,7 +24,8 @@ for liar_strategy in ${LIAR_STRATEGIES[@]}; do
             mpirun -np 8 python -m scalbo.exp --problem $problem \
                 --search AMBS \
                 --timeout $timeout \
-                --liar-strategy $liar_strategy \
+                --acq-func $acq_func \
+                --strategy $strategy \
                 --random-state $random_state \
                 --log-dir $log_dir \
                 --verbose 1 \

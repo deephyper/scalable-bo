1bb8bb5b5bc568a7625308d724199113b63b9d45
Deathn0t <romainegele@gmail.com>
2022-03-18 10:12:23 +0000

    tmp

--- src/scalbo/scalbo/benchmark/molecularmpnn.py
+++ src/scalbo/scalbo/benchmark/molecularmpnn.py
@@ -9,9 +9,25 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 from mpi4py import MPI
-os.environ["CUDA_VISILBE_DEVICES"] = str(MPI.COMM_WORLD.Get_rank())
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+gpu_local_id = rank % size
+os.environ["CUDA_VISILBE_DEVICES"] = str(gpu_local_id)
+print(f"CUDA_VISIBLE_DEVICES={gpu_local_id}")
 
 import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np

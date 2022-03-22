aaa585fd6f9548323f732964eb504ef7112b4cd7
Deathn0t <romainegele@gmail.com>
2022-03-22 08:46:50 +0100

    update mps service

---
+++ experiments/thetagpu/jobs/molecular-DMBS-async-qUCB-qUCB-1-16-1800-42.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#COBALT -n 1
+#COBALT -t 45
+#COBALT -q bigmem
+#COBALT -A datascience
+
+source ../../../build/activate-dhenv.sh
+
+
+
+export RANKS_PER_NODE=16
+export COBALT_JOBSIZE=1
+export PYTHONPATH=../../../build/dhenv/lib/python3.8/site-packages/:$PYTHONPATH
+
+export acq_func="qUCB"
+export strategy="qUCB"
+export timeout=1800
+export random_state=42 
+export problem="molecular"
+export sync_val=0
+export search="DMBS"
+
+if [[ "$sync_val" -eq 0 ]];
+then
+  export sync_str="async"
+else
+  export sync_str="sync"
+fi
+
+# For MPS Client Application
+export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps 
+export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
+
+# AMBS
+export log_dir="output/$problem-$search-$sync_str-$acq_func-$strategy-$COBALT_JOBSIZE-$RANKS_PER_NODE-$timeout-$random_state";
+
+echo "mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -np $(( $COBALT_JOBSIZE * $RANKS_PER_NODE )) --hostfile $COBALT_NODEFILE python -m scalbo.exp --problem $problem \
+--search $search \
+--sync $sync_val \
+--timeout $timeout \
+--acq-func $acq_func \
+--strategy $strategy \
+--random-state $random_state \
+--log-dir $log_dir \
+--verbose 1";
+
+mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -np $(( $COBALT_JOBSIZE * $RANKS_PER_NODE )) --hostfile $COBALT_NODEFILE python -m scalbo.exp --problem $problem \
+    --search $search \
+    --sync $sync_val \
+    --timeout $timeout \
+    --acq-func $acq_func \
+    --strategy $strategy \
+    --random-state $random_state \
+    --log-dir $log_dir \
+    --verbose 1 

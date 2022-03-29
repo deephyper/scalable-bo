ed7d3f7546dd28bb9deaca0fc435924c02992911
Deathn0t <romainegele@gmail.com>
2022-03-29 23:16:21 +0200

    update

--- experiments/theta/jobs/submit-all-benchmark.sh
+++ experiments/theta/jobs/submit-all-benchmark.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-hartmann6D-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
-hartmann6D-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh
+qsub hartmann6D-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
+qsub hartmann6D-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh
 
-levy-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
-levy-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh
+qsub levy-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
+qsub levy-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh
 
-griewank-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
-griewank-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh
+qsub griewank-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
+qsub griewank-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh
 
-schwefel-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
-schwefel-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh
+qsub schwefel-DMBS-async-qUCB-qUCB-128-32-1800-42.sh
+qsub schwefel-DMBS-sync-UCB-boltzmann-128-32-1800-42.sh

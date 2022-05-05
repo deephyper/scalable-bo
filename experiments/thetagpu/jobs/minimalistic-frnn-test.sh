09a407565b66d9399f7398b1d55e15dae7c72f08
Joceran <jgouneau@thetalogin6.tmi.alcf.anl.gov>
2022-05-05 01:13:42 +0000

    minimalistic_frnn submit script

--- experiments/thetagpu/jobs/minimalistic-frnn-test.sh
+++ experiments/thetagpu/jobs/minimalistic-frnn-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #COBALT -n 1
-#COBALT -t 140
-#COBALT -q full-node
+#COBALT -t 15
+#COBALT -q single-gpu
 #COBALT -A datascience
+#COBALT --attrs filesystems=home,theta-fs0
 
 source ../../../build/activate-dhenv.sh
 
@@ -10,6 +11,7 @@ export RANKS_PER_NODE=1
 export COBALT_JOBSIZE=1
 export PYTHONPATH=../../../build/dhenv/lib/python3.8/site-packages/:$PYTHONPATH
 
+export log_dir="output/single-run"
 
 echo "Running: mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -n $(( $COBALT_JOBSIZE * $RANKS_PER_NODE )) --hostfile $COBALT_NODEFILE python -m scalbo.benchmark.minimalistic_frnn"
 mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -np $(( $COBALT_JOBSIZE * $RANKS_PER_NODE )) --hostfile $COBALT_NODEFILE python -m scalbo.benchmark.minimalistic_frnn

43787d3f7043eb3d521a3beb4e9383b7d28a2588
Deathn0t <romainegele@gmail.com>
2022-05-12 07:58:07 +0000

    update thetagpu/candle scripts

--- experiments/thetagpu/candle/candle_attn-HB-async-DUMMY-UCB-cl_max-8-8-25200-42.sh
+++ experiments/thetagpu/candle/candle_attn-HB-async-DUMMY-UCB-cl_max-8-8-25200-42.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-#COBALT -n 1
-#COBALT -t 190
-#COBALT -q bigmem
+#COBALT -n 8
+#COBALT -t 425
+#COBALT -q full-node
 #COBALT -A datascience
 
 #!!! CONFIGURATION - START
-export RANKS_PER_NODE=16
-export COBALT_JOBSIZE=1
-export acq_func="qUCB"
-export strategy="qUCB"
-export timeout=10800
+export RANKS_PER_NODE=8
+export COBALT_JOBSIZE=8
+export acq_func="UCB"
+export strategy="cl_max"
+export model="DUMMY"
+export timeout=25200
 export random_state=42 
 export problem="candle_attn"
 export sync_val=0
-export search="DMBS"
+export search="HB"
 #!!! CONFIGURATION - END
 
 # activate Python environment
 source ../../../build/activate-dhenv.sh
 export PYTHONPATH=../../../build/dhenv/lib/python3.8/site-packages/:$PYTHONPATH
 
-# start MPS daemon on each node
-./launch-mps-service.sh
+# # start MPS daemon on each node
+# ./launch-mps-service.sh
 
-# For MPS Client Application
-export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps 
-export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
+# # For MPS Client Application
+# export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps 
+# export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
 
 if [[ "$sync_val" -eq 0 ]];
 then
@@ -35,10 +36,15 @@ else
 fi
 
 # AMBS
-export log_dir="output/$problem-$search-$sync_str-$acq_func-$strategy-$COBALT_JOBSIZE-$RANKS_PER_NODE-$timeout-$random_state";
+export log_dir="output/$problem-$search-$sync_str-$model-$acq_func-$strategy-$COBALT_JOBSIZE-$RANKS_PER_NODE-$timeout-$random_state";
 
-echo "mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -n $(( $COBALT_JOBSIZE * $RANKS_PER_NODE )) -N $RANKS_PER_NODE --hostfile $COBALT_NODEFILE python -m scalbo.exp --problem $problem \
+# profile gpu
+mkdir $log_dir;
+gpustat --no-color -i >> "$log_dir/gpustat.txt" &
+
+echo "mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -np $(( $COBALT_JOBSIZE * $RANKS_PER_NODE )) --hostfile $COBALT_NODEFILE python -m scalbo.exp --problem $problem \
 --search $search \
+--model $model \
 --sync $sync_val \
 --timeout $timeout \
 --acq-func $acq_func \
@@ -49,6 +55,7 @@ echo "mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -n $(( $COBALT_JOBSIZE * $
 
 mpirun -x LD_LIBRARY_PATH -x PYTHONPATH -x PATH -n $(( $COBALT_JOBSIZE * $RANKS_PER_NODE )) -N $RANKS_PER_NODE --hostfile $COBALT_NODEFILE python -m scalbo.exp --problem $problem \
     --search $search \
+    --model $model \
     --sync $sync_val \
     --timeout $timeout \
     --acq-func $acq_func \
